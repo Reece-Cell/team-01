3997e2ee762a190dbc2e3d71404b92d5fd91144a
Jesus Morfin <jmorfin@csumb.edu>
2023-09-12 19:06:50 -0700

    Transition Scene From Main Menu to Game
    I created a main menu for the game.

---
+++ Assets/Scripts/MainMenu/TransitionScene.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class TransitionScene : MonoBehaviour
+{
+    public Image img;
+    public AnimationCurve curve;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+    
+    IEnumerator FadeIn()
+    {
+        
+        float t = 1f;
+
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(img.color.r, img.color.g, img.color.b, a);
+            yield return 0;
+        }
+    }
+    
+    IEnumerator FadeOut(string scene)
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(img.color.r, img.color.g, img.color.b, a);
+            yield return 0;
+        }
+
+        SceneManager.LoadScene(scene);
+    }
+}
